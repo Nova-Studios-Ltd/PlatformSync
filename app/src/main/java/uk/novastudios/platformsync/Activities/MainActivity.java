1c9124481831d7102b4c4ee9e226d7f06fc17512
GentlyTech <andyyep@hotmail.com>
2023-12-21 14:49:00 -0500

    Setup ActivityMainBinding

--- app/src/main/java/uk/novastudios/platformsync/Activities/MainActivity.java
+++ app/src/main/java/uk/novastudios/platformsync/Activities/MainActivity.java
@@ -1,16 +1,18 @@
 package uk.novastudios.platformsync.Activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import uk.novastudios.platformsync.R;
+import androidx.appcompat.app.AppCompatActivity;
+
+import uk.novastudios.platformsync.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
