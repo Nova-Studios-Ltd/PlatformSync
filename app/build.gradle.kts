247792ab194b450f910be4dffb5c43be98fb1bd5
GentlyTech <andyyep@hotmail.com>
2023-12-21 14:40:38 -0500

    Add Navigation library Enable buildConfig and viewBinding in buildFeatures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,13 +18,23 @@ android {
 
     buildTypes {
         release {
+            buildConfigField("Boolean", "DEBUG", "false")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+        debug {
+            buildConfigField("Boolean", "DEBUG", "true")
+        }
     }
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -32,10 +42,12 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,12 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
 }
+
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.6")
+    }
+}
